b39ef2b3104935998cf77483129af97f1d7527e6
Oleg <olegmanu9@gmail.com>
2023-06-02 21:05:33 +0300

    release version

--- app/src/main/java/com/example/weatherapp/InitialActivity.kt
+++ app/src/main/java/com/example/weatherapp/InitialActivity.kt
@@ -3,18 +3,11 @@ package com.example.weatherapp
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.location.Location
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.weatherapp.view.adapters.MainHourlyListAdapter
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 const val GEO_LOCATION_REQUEST_COD_SUCCESS = 1
@@ -25,21 +18,6 @@ class InitialActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_initial)
-
-//        requestPermission()
-//
-//        val permissionGranted = ActivityCompat.checkSelfPermission(
-//            this, Manifest.permission.ACCESS_FINE_LOCATION
-//        ) == PackageManager.PERMISSION_GRANTED
-//        if (permissionGranted) {
-//            val intent = Intent(this, MainActivity::class.java)
-//            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-//            startActivity(intent)
-//        } else {
-//            requestPermission()
-//        }
-        checkPermission()
-
         val permissionGranted = ActivityCompat.checkSelfPermission(
             this, Manifest.permission.ACCESS_FINE_LOCATION
         ) == PackageManager.PERMISSION_GRANTED
@@ -50,18 +28,8 @@ class InitialActivity : AppCompatActivity() {
         } else {
             checkPermission()
         }
-
-
-
     }
 
-    private fun requestPermission() {
-        ActivityCompat.requestPermissions(
-            this, arrayOf(
-                Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
-            ), LOCATION_RC
-        )
-    }
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
